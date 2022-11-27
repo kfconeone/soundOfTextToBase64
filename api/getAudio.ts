e6a1bc082542bb32e3acf1e668f16277905b5714
Kevin <kfconeone@gmail.com>
2022-11-27 12:09:30 +0800

    init3

--- api/getAudio.ts
+++ api/getAudio.ts
@@ -4,11 +4,12 @@ import ab2str from "arraybuffer-to-string";
 import str2ab from "string-to-arraybuffer";
 
 async function fetchAudio(url: string) {
-    var result = await fetch("https://files.soundoftext.com/b7155c50-6dfc-11ed-a44a-8501b7b1aefa.mp3");
-    let decoder = new TextDecoder();
-    let data = await result.arrayBuffer();
+    // var result = await fetch("https://files.soundoftext.com/b7155c50-6dfc-11ed-a44a-8501b7b1aefa.mp3");
+    // let decoder = new TextDecoder();
+    // let data = await result.arrayBuffer();
 
-    return ab2str(data, "base64");
+    // return ab2str(data, "base64");
+    return "asdasd";
 }
 
 export default async (request: VercelRequest, response: VercelResponse) => {
