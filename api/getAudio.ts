9369045cc64de728d34ef3ccde9fe1fc3e562173
Kevin <kfconeone@gmail.com>
2022-11-27 12:16:59 +0800

    init4

--- api/getAudio.ts
+++ api/getAudio.ts
@@ -4,12 +4,11 @@ import ab2str from "arraybuffer-to-string";
 import str2ab from "string-to-arraybuffer";
 
 async function fetchAudio(url: string) {
-    // var result = await fetch("https://files.soundoftext.com/b7155c50-6dfc-11ed-a44a-8501b7b1aefa.mp3");
-    // let decoder = new TextDecoder();
-    // let data = await result.arrayBuffer();
+    var result = await fetch("https://files.soundoftext.com/b7155c50-6dfc-11ed-a44a-8501b7b1aefa.mp3");
+    let decoder = new TextDecoder();
+    let data = await result.arrayBuffer();
 
-    // return ab2str(data, "base64");
-    return "asdasd";
+    return ab2str(data, "base64");
 }
 
 export default async (request: VercelRequest, response: VercelResponse) => {
